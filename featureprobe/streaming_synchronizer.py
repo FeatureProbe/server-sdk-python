ac99aea8d992115205c8da4f7fdbd4a3c2fbf8e4
jianggang <gangb.info@gmail.com>
2023-04-21 19:11:20 +0800

    update ci yml

--- featureprobe/streaming_synchronizer.py
+++ featureprobe/streaming_synchronizer.py
@@ -19,6 +19,8 @@ from urllib.parse import urlparse
 
 from featureprobe.realtime import RealtimeToggleUpdateNS
 
+from sys import version_info as python_version
+
 if python_version >= (3, 6):
     import socketio
 
